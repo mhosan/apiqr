d09d507a86a9373de296ada90f54bb44d9c6fa0f
marcelo <mhosan@gmail.com>
2020-12-16 00:10:34 -0300

    armando la cosa

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,41 @@
 const { Router } = require('express') // traer una parte de express
 const router = Router()
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const suscripcionesEsquema = require('../modelos/subsc')
-const mensajesEsquema = require('../modelos/msg')
-const categoriasEsquema = require('../modelos/category')
-const ctrlSubsc = require('../controladores/ctrlSubsc')
 const ctrlPagPpal = require('../controladores/ctrlPagPpal')
-const ctrlSubscripciones = require('../controladores/ctrlSubsc')
-const ctrlMsg = require('../controladores/ctrlMsg')
-
-let pushSubscription
-let subscripcionDestino
 
 // ---------------------------------------------------------------------
 // pagina principal de adm del servidor. Esta página probablemente
 // desaparezca
 // ---------------------------------------------------------------------
-// router.get('/', ctrlPagPpal.getSubscripciones);
-router.get('/', (req, res) => {
-  res.render('template', { saludo: 'hola mundo' })
-})
+router.get('/', ctrlPagPpal.getSubscripciones);
 
 // ---------------------------------------------------------------------
 // recibiendo (y persistiendo) subscripción
 // ---------------------------------------------------------------------
-router.post('/subscription', ctrlSubscripciones.postSubscripcion)
+//router.post('/subscription', ctrlSubscripciones.postSubscripcion)
 
 // ---------------------------------------------------------------------
 // recibiendo y actualizando la subscripción. Se le agregan datos para
 // identificar al usuario, por ahora es el mail.
 // ---------------------------------------------------------------------
-router.put('/', ctrlSubscripciones.putSubscripcion)
+//router.put('/', ctrlSubscripciones.putSubscripcion)
 
 // ---------------------------------------------------------------------
 // listar todas las subscripciones
 // ---------------------------------------------------------------------
-router.get('/subscripciones', ctrlSubsc.getSubscripciones)
+//router.get('/subscripciones', ctrlSubsc.getSubscripciones)
 
 // ---------------------------------------------------------------------
 // borrar suscripcion. se recibe como param el codigo auth
 // OJO, esto se ejecuta desde una llamada request a esta API
 // ---------------------------------------------------------------------
-router.delete('/delete/:auth', ctrlSubscripciones.deleteSubscripcion)
+//router.delete('/delete/:auth', ctrlSubscripciones.deleteSubscripcion)
 
 // ---------------------------------------------------------------------
 // borrar suscripcion. se recibe como param el codigo auth
 // OJO, esto se ejecuta desde el código main en el cliente!. NO es una
 // llamada API request
 // ---------------------------------------------------------------------
-router.put('/borrar', ctrlSubscripciones.borrarSubscripcion)
+//router.put('/borrar', ctrlSubscripciones.borrarSubscripcion)
 
 // ---------------------------------------------------------------------
 // mh, 24/11/20
@@ -64,17 +50,17 @@ router.put('/borrar', ctrlSubscripciones.borrarSubscripcion)
 //                  la categoria.
 // 5 status ?       Se habló de un item "status" pero por ahora no está definido.
 // ---------------------------------------------------------------------
-router.post('/new-message', ctrlMsg.newMessage)
+//router.post('/new-message', ctrlMsg.newMessage)
 
 // ---------------------------------------------------------------------
 // post new-message con sender
 // ---------------------------------------------------------------------
-router.post('/new-message-sender', ctrlMsg.newMessageSender)
+//router.post('/new-message-sender', ctrlMsg.newMessageSender)
 
 // ---------------------------------------------------------------------
 // listar notificaciones a partir de un mail
 // { 'mail' : 'algo@gmail.com'}
 // ---------------------------------------------------------------------
-router.get('/listado/:mail', ctrlMsg.listarNotificaciones)
+//router.get('/listado/:mail', ctrlMsg.listarNotificaciones)
 
 module.exports = router
