9af0100e08cb8088bafed44993d7a594de773b6d
marcelo <mhosan@gmail.com>
2020-12-18 23:42:50 -0300

    nada

--- controladores/ctrlPagPpal.js
+++ controladores/ctrlPagPpal.js
@@ -8,12 +8,10 @@ const ctrlPagPpal = {}
 //---------------------------------------------------------------------
 ctrlPagPpal.getCobros = async (req, res) => {
   jsonCobros = [];
-
-  cobrosSchema.find().exec()
+  let i = 0;
+  cobrosSchema.find().sort({monto:1}).exec()  //
     .then(doc => {
-      let i = 0;
       doc.forEach(element => {
-        
         let fechaMongo = new Date(element.fechaOperacionInicio);
         fechaLocal = fechaMongo.toLocaleString('es-ES');
         estadosSchema.find({ 'tipo': element.estado }).exec()
@@ -34,9 +32,9 @@ ctrlPagPpal.getCobros = async (req, res) => {
                 i = i + 1;
                 //ver si es la ultima vuelta
                 if (i === doc.length) {
+                  console.log(jsonCobros)
                   res.render('template', { cobros: jsonCobros })
                 }
-
               })
               .catch(err => {
                 console.log(`Error al buscar el tipo de transaccion: ${err}`)
@@ -45,54 +43,11 @@ ctrlPagPpal.getCobros = async (req, res) => {
           .catch(err => {
             console.log(`Error al buscar el estado: ${err}`)
           })
-
       })  //<---foreach
-
     })  //<---then ppal.
     .catch(err => {
-      console.log(`Error: ${err}`);
+      console.log(`Error en la query sobre la tabla cobros: ${err}`);
     })
-
-
-  // qrEstados.find().exec()
-  //   .then(doc => {
-  //     console.log(doc);
-  //     //console.log(doc[0].tipo);
-  //     qrEsquema.find().exec()
-  //       .then(cobros => {
-  //         cobros.forEach(element => {
-  //           let fechaMongo = new Date(element.fechaOperacionInicio);
-  //           fechaLocal = fechaMongo.toLocaleString('es-ES');
-  //           //setear estado
-  //           doc.forEach(element => {
-  //             // if (element.tipo === 1){
-  //             //   console.log(`lo encontré: ${element.tipo}`)
-  //             // }
-  //             console.log(element)
-  //           });
-  //           const elementoJson = {
-  //             receiver: element.receiver,
-  //             monto: element.monto,
-  //             tipoTransaccion: element.tipoTransaccion,
-  //             estado: element.estado,
-  //             sender: element.sender,
-  //             fecha: fechaLocal
-  //           }
-  //           jsonCobros.push(elementoJson);
-  //         });
-  //         res.render('template', { cobros: jsonCobros })
-  //       })
-  //       .catch(err => {
-  //         console.log(`Error al leer los cobros`);
-  //       });
-  //   })
-  //   .catch(err => {
-  //     console.log(`Error ${err}`)
-
-  //   })
-}
-const enviarResultados = (res, resultados) => {
-  res.render('template', { cobros: resultados });
 }
 
 module.exports = ctrlPagPpal
