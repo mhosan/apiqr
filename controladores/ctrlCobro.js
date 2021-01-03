61d0d2bc74a9e0794b2fea412d0fe51317d9d62d
marcelo <mhosan@gmail.com>
2021-01-02 22:43:19 -0300

    corrigiendo put de cobro!

--- controladores/ctrlCobro.js
+++ controladores/ctrlCobro.js
@@ -1,4 +1,5 @@
 const { Mongoose } = require('mongoose');
+const cobros = require('../modelos/cobros');
 const cobrosEsquema = require('../modelos/cobros')
 const ctrlCobro = {}
 
@@ -22,58 +23,133 @@ ctrlCobro.putCobroAlter = (req, res) => {
 
 
 //---------------------------------------------------------------------
+// verificar el mail
+//---------------------------------------------------------------------
+const verificarMail = (mailVerificar) => {
+  return new Promise((resolve, reject) => {
+    cobrosEsquema.find({ receiver: mailVerificar }, (err, res) => {
+      if (err) reject('error en la busqueda del mail')
+      if (res.length === 0) {
+        resolve(false);
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
+//---------------------------------------------------------------------
+// verificar el objeto monto
+//---------------------------------------------------------------------
+const verificarObjMonto = (objetoMontoVerificar) => {
+  let verificacion = "";
+  return new Promise((resolve, reject) => {
+    const objMontoCantElementos = objetoMontoVerificar.length;
+    if (!objMontoCantElementos === 0) {
+      objetoMontoVerificar.forEach(elementoObjMonto => {
+        if (!elementoObjMonto.monto) {
+          console.log("El objmonto no tiene el elemento monto.");
+          verificacion = "El objmonto no tiene el elemento monto.";
+        }
+        if (!elementoObjMonto.tipoTransaccion) {
+          console.log("El objmonto no trae el elemento tipoTransaccion.");
+          verificacion = "El objmonto no trae el elemento tipoTransaccion.";
+        }
+      })
+    }
+    let objMontoActual = [];
+    if (Array.isArray(objetoMontoVerificar) && objetoMontoVerificar.length) {  //<--el objeto objmonto no está vacio
+      verificacion = 'ok';
+      // req.body.objmonto.forEach(element => {
+      //   const objetoMonto = {
+      //     monto: element.monto,
+      //     tipoTransaccion: element.tipoTransaccion
+      //   };
+      //   objMontoActual.push(objetoMonto);
+      // });
+    } else {
+      //console.log(`El objeto monto esta vacio`);
+      verificacion = "El objeto monto esta vacio";
+    }
+    if (verificacion === 'ok') {
+      resolve(verificacion)
+    } else {
+      reject(verificacion)
+    }
+  })
+}
+
+//---------------------------------------------------------------------
+// ejecutar la query (put) de actualización una vez verificados 
+// todos los parametros (mail y objmonto)
+//---------------------------------------------------------------------
+const ejecutarPut = (mailRecibido, objetoMontoRecibido) => {
+  return new Promise((resolve, reject) => {
+    const filter = { 'receiver': mailRecibido, 'estado': 1 };
+    const actuObjMonto = { 'objmonto': objetoMontoRecibido }
+    cobrosEsquema.findOneAndUpdate(filter, actuObjMonto, { sort: { fechaOperacionInicio: -1 } }, (err, doc) => {
+      if (err) {
+        //console.log(`Error al actualizar el estado del cobro del mail ${mail}. El error es: ${err}`);
+        reject('Error al actualizar el estado del cobro');
+      } else {
+        //console.log(doc);
+        resolve(doc);
+      }
+    })
+  })
+}
+
+//---------------------------------------------------------------------
 // put del cobro por qr. Recibe dos parametros: mail receiver y objmonto.
 // y actualiza solamente objmonto. No actualiza estado.
 //---------------------------------------------------------------------
 ctrlCobro.putCobro = (req, res) => {
-  let postError = "";
-  let objMontoActual = [];
-  const objMontoCantElementos = req.body.objmonto.length;
   if (!req.body.mail) {
-    postError = postError + " El Json no trae el elemento mail receiver.";
-  }
-  if (!objMontoCantElementos === 0) {
-    req.body.objmonto.forEach(elementoObjMonto => {
-      if (!elementoObjMonto.monto) {
-        postError = postError + " El Json no trae el elemento monto.";
-      }
-      if (!elementoObjMonto.tipoTransaccion) {
-        postError = postError + " El Json no trae el elemento tipoTransaccion.";
-      }
-    })
-  }
-  if (!postError == "") {
-    console.log(postError);
-    res.status(420).json(`Error: ${postError}`);
+    console.log(" El Json no trae el elemento mail.");
+    res.status(404).json(`Error: falta el mail`);
     return;
   }
-  if(Array.isArray(req.body.objmonto) && req.body.objmonto.length){  //<--el objeto objmonto no está vacio
-    req.body.objmonto.forEach(element => {
-      const objetoMonto = {
-        monto: element.monto,
-        tipoTransaccion: element.tipoTransaccion
-      };
-      objMontoActual.push(objetoMonto);
-    });
-  } else {
-    objMontoActual = [];
+  if (!req.body.objmonto) {
+    console.log("El json no trae el objeto objmonto")
+    res.status(404).json('Error: falta el objeto objmonto');
+    return;
   }
-    
-  const mail = req.body.mail;
-  //const nuevoEstado = req.body.estado;
-  const filter = { 'receiver': mail, 'estado': 1 };
-  const actuObjMonto = { 'objmonto': objMontoActual }
-
-  cobrosEsquema.findOneAndUpdate(filter, actuObjMonto, { sort: { fechaOperacionInicio: -1 } }, (err, doc) => {
-    if (err) {
-      console.log(`Error al actualizar el estado del cobro del mail ${mail}. El error es: ${err}`);
-      res.status(404).json(`Error al actualizar el estado del cobro del mail ${mail}. El error es: ${err}`);
-    } else {
-      console.log(`Actualización Ok del estado del cobro`);
-      res.status(200).json(`Actualización del estado del cobro ok!`);
-    }
-  })
+  verificarMail(req.body.mail)
+    .then(respuesta => {
+      if (respuesta) {
+        console.log('mail verificado ok!')
+        verificarObjMonto(req.body.objmonto)
+          .then(respuestaVerifMonto => {
+            console.log(`monto verif. ok: ${respuestaVerifMonto}`);
+            ejecutarPut(req.body.mail, req.body.objmonto)
+              .then(resultadoQuery => {
+                if(resultadoQuery === null){
+                  res.status(200).json(`No se actualizó ningún registro. Verificar que estado = pendiente`);
+                } else {
+                  res.status(200).json('Actualización ok!')
+                }
+              })
+              .catch(err => {
+                console.log(`error: ${err}`);
+                res.status(404).json(`Error al actualizar el estado del cobro del mail`);
+              })
+          })
+          .catch(err => {
+            console.log(`error al verificar el monto: ${err}`);
+            res.status(404).json(`Error con el objmonto: ${err}`);
+          })
+      } else {
+        console.log('El mail no se encuentra en la bd');
+        res.status(404).json('El mail no se encuentra en la bd')
+        return;
+      }
+    })
+    .catch(err => {
+      console.log('error en la verificacion del mail')
+    })
 }
+
+
 
 //---------------------------------------------------------------------
 // get del cobro por qr
